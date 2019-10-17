5e9d5ca836bf9d534d8cfd9b0318ce6d3944e47b
Charles Danner <danner.ced1@gmail.com>
2019-10-17 17:05:50 -0400

    got it set up to actually start the update button. logging the id that I need.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
     const textArea = document.getElementById('textArea');
+    const devourBtn = document.getElementsByClassName('devourBtn')
 
     document.getElementById('submit').addEventListener('click', () => {
         const newBurger = textArea.value.trim()
+        document.getElementById('error').style.display = 'none';
 
         if (textArea.value.trim() != "") {
 
@@ -11,9 +13,17 @@ document.addEventListener("DOMContentLoaded", function (event) {
                 .then(response => {
                     response.data.burgerExists ?   console.log('this shit is fucked') : location.reload();          
                 })
+        } else {
+            document.getElementById('error').style.display = 'block'
+            document.getElementById('error').style.color = 'red';
         }
     })
 
+   for(var i = 0; i < devourBtn.length; i++){
+       devourBtn[i].addEventListener('click', function() {
+           console.log(this.id)
+       })
+   }
 
 
 })
