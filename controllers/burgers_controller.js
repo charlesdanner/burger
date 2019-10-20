d95d1a127e2fe1dafeae9ac4c04fc2ee58e0ba39
Charles Danner <danner.ced1@gmail.com>
2019-10-20 17:27:18 -0400

    working on setting up burger.js file to communicate correctly with the burger.js file. currently have the get request working properly.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,32 +1,40 @@
 const express = require('express');             //dependencies being loaded.
-const app = express();
-const orm = require('./../config/orm');
+const router = express.Router();
+const burger = require('../models/burger.js');
 
 
-module.exports = app => {
 
-    app.get('/', (req, res) => {
-        orm.selectAll('burgers', data => {          //when a get request is made on the root orm.selectAll runs and renders the results through the handlebars template, index
-            res.render('index', data)
-        })
+router.get('/', (req, res) => {
+    burger.all(function(data){
+        const object = {
+            burgers: data
+        }
+        console.log(object)
+        res.render('index', object)
     })
+    // orm.selectAll('burgers', data => {          //when a get request is made on the root orm.selectAll runs and renders the results through the handlebars template, index
+    //     res.render('index', data)
+    // })
+})
 
-    app.post('/api/new-burger', (req, res) => {                   //when a post request is made...
-        const newBurger = req.body.newBurger
-        orm.checkIfExists('burgers', newBurger, filter => {        //orm.checkIfExists runs to verify if a requested burger already exists within the table
-            console.log(filter)
-            if (filter.length === 0) {              //if the returned array if empty, the burger does not exist
-                orm.insertOne('burgers', 'burger_name', newBurger, () => res.json({ burgerExists: false }))      
-            } else res.json({ burgerExists: true })              //if burger doesn't exist orm.createNew runs to add a new entry into the table with the client's burger name
-        })                                                    //object returned if the burger name isn't already taken
-    })                                                           //object returned if the burger name is already taken
-    
-    app.put('/api/devour-burger/:id', (req, res) => {
-        const id = req.params.id;
-        orm.updateOne('burgers', 'devoured', true, 'id', id, (result) =>{
-            res.json(result.changedRows)
-        })
+router.post('/api/new-burger', (req, res) => {                   //when a post request is made...
+    const newBurger = req.body.newBurger
+    orm.checkIfExists('burgers', newBurger, filter => {        //orm.checkIfExists runs to verify if a requested burger already exists within the table
+        console.log(filter)
+        if (filter.length === 0) {              //if the returned array if empty, the burger does not exist
+            orm.insertOne('burgers', 'burger_name', newBurger, () => res.json({ burgerExists: false }))
+        } else res.json({ burgerExists: true })              //if burger doesn't exist orm.createNew runs to add a new entry into the table with the client's burger name
+    })                                                    //object returned if the burger name isn't already taken
+})                                                           //object returned if the burger name is already taken
+
+router.put('/api/devour-burger/:id', (req, res) => {
+    const id = req.params.id;
+    orm.updateOne('burgers', 'devoured', true, 'id', id, (result) => {
+        res.json(result.changedRows)
     })
-       
-}
+})
+
+module.exports = router
+
+
 
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()                               //server dependencies
-const exphbs = require('express-handlebars');
 
 const PORT = process.env.PORT || 3000;              //PORT 3000 for development, process.env.PORT for when deployed to a website like heroku
 
@@ -8,11 +7,13 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());                        //express parsing the req.body
 app.use(express.static('public'));              //setting the context route to the public folder, so the client has access to its contents
 
+const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));      //setting 'main' as the default layout for handlebars
 app.set('view engine', 'handlebars');                           //setting handlebars as the view engine for the site
 
-require('./controllers/burgers_controller')(app);       //requiring the routing information from burgers_controller.js
+const routes = require('./controllers/burgers_controller');       //requiring the routing information from burgers_controller.js
 
+app.use(routes)
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`)           //port listening 
