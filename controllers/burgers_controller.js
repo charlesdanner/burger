59b2768e5a8c020301c0753da738ee9650cd0c7f
Charles Danner <danner.ced1@gmail.com>
2019-10-20 22:17:16 -0400

    added some more basic styling to the app. added a title and some padding inside the divs containing the burger names.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,18 +5,13 @@ const burger = require('../models/burger.js');
 
 
 router.get('/', (req, res) => {
-    burger.all(function (data) {
-
+    burger.all(function(data) {
         res.render('index', data)
     })
-    // orm.selectAll('burgers', data => {          //when a get request is made on the root orm.selectAll runs and renders the results through the handlebars template, index
-    //     res.render('index', data)
-    // })
 })
 
 router.post('/api/new-burger', (req, res) => {                   //when a post request is made...
     const newBurger = req.body.newBurger
-
     burger.create('burgers', 'burger_name', newBurger, burgerExists => {
         res.json(burgerExists)
     })
@@ -28,9 +23,6 @@ router.put('/api/devour-burger/:id', (req, res) => {
     burger.update('burgers', 'devoured', true, 'id', id, function(result){
         res.json(result.changedRows)
     })
-    // orm.updateOne('burgers', 'devoured', true, 'id', id, (result) => {
-    //     res.json(result.changedRows)
-    // })
 })
 
 module.exports = router
